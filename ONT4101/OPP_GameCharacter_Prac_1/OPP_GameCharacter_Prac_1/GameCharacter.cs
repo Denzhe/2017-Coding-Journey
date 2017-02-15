935e273aeabc27378207156033ca94a19a2aec15
Lincoln <blazeboy.wachira@gmail.com>
2017-02-16 00:32:43 +0200

    First Push

---
+++ ONT4101/OPP_GameCharacter_Prac_1/OPP_GameCharacter_Prac_1/GameCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_GameCharacter_Prac_1
+{
+    public class GameCharacter
+    {
+        public const int maxWidthArea = 300;
+        public const int maxheightArea = 300;
+
+        protected int currentX;
+        protected int currentY;
+        private string symbol;
+
+        
+
+        public GameCharacter(int currentX, int currentY,string symbol)
+        {
+            this.currentX = currentX;
+            this.currentY = currentY;
+            this.symbol = symbol;
+        }
+
+   
+
+
+    }
+}
